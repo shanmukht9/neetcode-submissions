db5a7f633d81e382728250194c49fbfa1939bd49
shanmukht9 <shanmukht929@gmail.com>
2026-08-03 11:09:22 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for(char c:s.toCharArray()){
+            if(c=='(') stk.push(')');
+            else if(c=='{') stk.push('}');
+            else if(c=='[') stk.push(']');
+
+            else if(stk.isEmpty() || stk.pop!=c) return false;
+
+        }
+
+        return true;
+    }
+}
