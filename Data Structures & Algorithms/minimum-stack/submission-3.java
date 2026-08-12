55d85f529af33d337db7b26b8c0c7fd39ed7461e
shanmukht9 <shanmukht929@gmail.com>
2026-08-12 14:21:02 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,28 @@
+class MinStack {
+    Stack<Integer> stk;
+    Stack<Integer> minstk;
+
+    public MinStack() {
+        stk = new Stack<>();
+        minstk = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stk.push(val);
+        if(minstk.isEmpty()||val<=minstk.peek()) minstk.push(val);
+    }
+    
+    public void pop() {
+        if(stk.isEmpty()) return;
+        int top = stk.pop();
+        if(top==minstk.peek()) minstk.pop();
+    }
+    
+    public int top() {
+        return stk.peek();
+    }
+    
+    public int getMin() {
+        return minstk.peek();
+    }
+}
